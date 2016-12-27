cc27aaa1f286f9130b612c4712866201d1bfa332
Peter Bae <pskbae@gmail.com>
2016-12-27 07:25:07 -0800

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ ln -si $(dirname $(realpath $0))/.tmux.conf $HOME
 ln -si $(dirname $(realpath $0))/.psqlrc $HOME
 ln -si $(dirname $(realpath $0))/.zshrc $HOME
 ln -si $(dirname $(realpath $0))/.bashrc $HOME
+ln -si $(dirname $(realpath $0))/.gitconfig $HOME
+
