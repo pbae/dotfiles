67722e604348115efb72a8762ba3b3c3c71a0ccc
Peter Bae <pskbae@gmail.com>
2016-12-27 06:29:01 -0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -si `dirname $0`/.vimrc $HOME
+ln -si `dirname $0`/.pryrc $HOME
+ln -si `dirname $0`/.tmux.conf $HOME
+ln -si `dirname $0`/.psqlrc $HOME
+ln -si `dirname $0`/.zshrc $HOME
+ln -si `dirname $0`/.bashrc $HOME
+
