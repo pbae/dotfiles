b8dad852be1afb7efc35acc1990400e6e6a52b69
Peter Bae <pskbae@gmail.com>
2016-12-27 07:24:25 -0800

    Fix last commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-ln -si `dirname $0`/.vimrc $HOME
-ln -si `dirname $0`/.pryrc $HOME
-ln -si `dirname $0`/.tmux.conf $HOME
-ln -si `dirname $0`/.psqlrc $HOME
-ln -si `dirname $0`/.zshrc $HOME
-ln -si `dirname $0`/.bashrc $HOME
-
+ln -si $(dirname $(realpath $0))/.vimrc $HOME
+ln -si $(dirname $(realpath $0))/.tmux.conf $HOME
+ln -si $(dirname $(realpath $0))/.psqlrc $HOME
+ln -si $(dirname $(realpath $0))/.zshrc $HOME
+ln -si $(dirname $(realpath $0))/.bashrc $HOME
